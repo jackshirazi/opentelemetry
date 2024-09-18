2dfae52f19e29ad34e01abcf393b19bcf0395270
Jack Shirazi <jack.shirazi@elastic.co>
2024-09-18 18:38:16 +0100

    add java pod start

--- test/operator/wait_for_pod_start.sh
+++ test/operator/wait_for_pod_start.sh
@@ -4,24 +4,26 @@ set -euxo pipefail
 
 MAX_WAIT_SECONDS=60
 NAMESPACE=$1
-GREP=$2
+POD_NAME=$2
+GREP=$3
+COUNT_PODS=$4
 
-echo "Waiting an initial 10 seconds for the $NAMESPACE pods to start"
+echo "Waiting an initial 10 seconds for the $NAMESPACE pods called $POD_NAME to start"
 sleep 10
-echo "Waiting up to $MAX_WAIT_SECONDS more seconds for the $NAMESPACE pods to be ready"
+echo "Waiting up to $MAX_WAIT_SECONDS more seconds for the $NAMESPACE pods called $POD_NAME to be ready"
 count=0
 while [ $count -lt $MAX_WAIT_SECONDS ]
 do
   count=`expr $count + 1`
-  STARTED=$(kubectl get pod -n $NAMESPACE | (grep "$GREP" || true) | wc -l)
-  if [ $STARTED -eq 0 ]
+  STARTED=$(kubectl get pod -n $NAMESPACE | (grep "$POD_NAME" || true) | (grep "$GREP" || true) | wc -l)
+  if [ $STARTED -eq $COUNT_PODS ]
   then
     exit 0
   fi
   sleep 1
 done
 
-echo "error: the $NAMESPACE pods failed to be ready within $MAX_WAIT_SECONDS seconds"
+echo "error: the $NAMESPACE pods called $POD_NAME failed to be ready within $MAX_WAIT_SECONDS seconds"
 echo "-- pod info:"
 kubectl get pod -A
 echo "--"
