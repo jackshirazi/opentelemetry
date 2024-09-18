dc06d91f7033e652a4022930b343b60101df718a
Jack Shirazi <jack.shirazi@elastic.co>
2024-09-18 17:26:22 +0100

    fix mixup

--- test/operator/wait_for_pod_start.sh
+++ test/operator/wait_for_pod_start.sh
@@ -6,7 +6,11 @@ MAX_WAIT_SECONDS=60
 NAMESPACE=$1
 GREP=$2
 
-echo "Waiting up to $MAX_WAIT_SECONDS seconds for the cert managers to start"
+set +u
+
+echo "Waiting an initial 10 seconds for the cert managers to start"
+sleep 10
+echo "Waiting up to $MAX_WAIT_SECONDS more seconds for the cert managers to start"
 count=0
 while [ $count -lt $MAX_WAIT_SECONDS ]
 do
